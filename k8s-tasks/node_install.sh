a523932228e128b6b4fc3b8645c8b79892a3f2ad
Roman_Tirskikh <roman_tirskikh@epam.com>
2017-10-10 12:08:52 +0300

    add k8s tasks (in progress)

---
+++ k8s-tasks/node_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo -i
+
+sysctl net.bridge.bridge-nf-call-iptables=1
+
+apt-get -y update && apt-get install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get -y update && apt-get install -y git openjdk-8-jre docker.io kubelet kubeadm kubectl kubernetes-cni
+
+#su - ubuntu | $(cat /vagrant/kube.txt | grep "kubeadm join") --skip-preflight-checks
+
+sleep 3
