04df4c2b98b8b1482b2f3dac8b2705b287d2ae45
RomanTirskikh <RomanTirskikh@epam.com>
2017-10-13 17:23:39 +0300

    work continue

--- k8s-tasks/grafana.sh
+++
@@ -1,3 +0,0 @@
-sudo su - ubuntu
-/usr/bin/kubectl create -f /home/ubuntu/heapster/deploy/kube-config/influxdb/ --openapi-validation=true && sleep 3
-/usr/bin/kubectl create -f /home/ubuntu/heapster/deploy/kube-config/rbac/heapster-rbac.yaml --openapi-validation=true && sleep 3
