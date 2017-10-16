9dae132d8a487991a692b40f7f7fe1fec0813796
RomanTirskikh <RomanTirskikh@epam.com>
2017-10-16 17:25:09 +0300

    continue work

---
+++ k8s-tasks/html/env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "<!DOCTYPE html>"
+echo ""
+echo '<html>'
+echo '<body>'
+echo ""
+echo '<h1>My First Heading</h1>'
+echo ""
+echo '<p>My first paragraph.</p>'
+echo ""
+echo '<p>this file store in /usr/share/nginx/html/</p>'
+echo ""
+echo '<p>'
+echo 'This pod name is: '$MYPOD_NAME
+echo '</p>'
+echo ""
+echo '</body>'
+echo '</html>'
